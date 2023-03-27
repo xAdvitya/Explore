d17e6111fed0ee83d8b7d877013d179f51104c71
xAdvitya <advityasharma21@gmail.com>
2023-03-28 04:07:14 +0530

    adding CORS

--- src/main/java/com/explore/explore/controller/MyController.java
+++ src/main/java/com/explore/explore/controller/MyController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.explore.explore.repository.SpotRepository;
 import com.explore.explore.repository.UserRepository;
 import com.explore.explore.utility.PasswordRequest;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MyController {
 
